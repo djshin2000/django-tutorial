e00dc8fa0dd6131b2ee6b87a96f20e6ce307783f
Shin Dongjin <djshin2000@gmail.com>
2018-02-02 16:46:27 +0900

    results 기능 구현

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -35,9 +35,17 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    choice_pk = request.POST['choice']
+    choice = Choice.objects.get(pk=choice_pk)
+    choice.votes += 1
+    choice.save()
+
+    return redirect('polls:results', question_id=question_id)
