7e02239557c888e6c054744b2b1d6156550e90b4
Shin Dongjin <djshin2000@gmail.com>
2018-02-02 15:10:56 +0900

    index.html 생성 및 view에 연결

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+    # render함수가 내부적으로 처리되는 과정
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, template))
 
 
 def detail(request, question_id):
